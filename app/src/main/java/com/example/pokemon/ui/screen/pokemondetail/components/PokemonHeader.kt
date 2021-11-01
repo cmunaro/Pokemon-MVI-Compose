83d0f99b0c4d634b3ec60c061a280ff745911f6f
munez07 <munaro07@gmail.com>
2021-11-01 01:58:33 +0100

    fix: glitches in detail screen

--- app/src/main/java/com/example/pokemon/ui/screen/pokemondetail/components/PokemonHeader.kt
+++ app/src/main/java/com/example/pokemon/ui/screen/pokemondetail/components/PokemonHeader.kt
@@ -1,5 +1,8 @@
 package com.example.pokemon.ui.screen.pokemondetail.components
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,30 +11,50 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.palette.graphics.Palette
 import com.example.pokemon.R
 import com.example.pokemon.data.domainmodel.Pokemon
+import com.example.pokemon.data.domainmodel.Type
 import com.example.pokemon.ui.shared.UnknownPokemonImage
+import com.example.pokemon.ui.theme.PokemonTheme
 import com.example.pokemon.ui.theme.gray_21
 import com.example.pokemon.utils.PokemonImageUtil
-import com.skydoves.landscapist.ShimmerParams
 import com.skydoves.landscapist.glide.GlideImage
 import com.skydoves.landscapist.palette.BitmapPalette
 
 @Composable
 fun PokemonHeader(pokemon: Pokemon?, disableImageFetching: Boolean = false) {
     var palette by remember { mutableStateOf<Palette?>(null) }
+    val backgroundColor = remember { Animatable(gray_21) }
+    LaunchedEffect(palette) {
+        palette?.dominantSwatch?.rgb?.let {
+            backgroundColor.animateTo(
+                targetValue = Color(it),
+                animationSpec = tween(
+                    durationMillis = 400,
+                    easing = LinearEasing
+                )
+            )
+        }
+    }
 
     Surface {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(200.dp)
+                .padding(bottom = 4.dp)
+                .shadow(
+                    elevation = 4.dp,
+                    shape = RoundedCornerShape(0, 0, 25, 25)
+                )
                 .clip(RoundedCornerShape(0, 0, 25, 25))
-                .background(color = palette?.dominantSwatch?.rgb?.let { Color(it) } ?: gray_21)
+                .background(color = backgroundColor.value)
         ) {
             if (pokemon == null || disableImageFetching) {
                 UnknownPokemonImage(modifier = Modifier.align(Alignment.Center))
@@ -44,19 +67,25 @@ fun PokemonHeader(pokemon: Pokemon?, disableImageFetching: Boolean = false) {
                     bitmapPalette = BitmapPalette {
                         palette = it
                     },
+                    loading = { UnknownPokemonImage(modifier = Modifier.align(Alignment.Center)) },
                     failure = { UnknownPokemonImage(modifier = Modifier.align(Alignment.Center)) },
                     contentScale = ContentScale.Fit,
                     previewPlaceholder = R.drawable.placeholder, // This is bugged
-                    imageModel = PokemonImageUtil.getImageUrlOf(pokemon.id),
-                    shimmerParams = ShimmerParams(
-                        baseColor = Color.Transparent,
-                        highlightColor = Color.LightGray,
-                        durationMillis = 400,
-                        dropOff = 0.8f,
-                        tilt = 20f
-                    )
+                    imageModel = PokemonImageUtil.getImageUrlOf(pokemon.id)
                 )
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PokemonHeaderPreview() {
+    val pokemon = Pokemon(
+        name = "Charmender",
+        types = listOf(Type.DRAGON, Type.FIRE, Type.ELECTRIC, Type.STEEL)
+    )
+    PokemonTheme {
+        PokemonHeader(pokemon, true)
     }
 }
